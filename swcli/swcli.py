a620a561389bb7c990146f949fd0d460b0a8b502
Pery Lemke <pery.lemke@gmail.com>
2021-10-22 13:57:31 -0300

    Improve try in get methods

--- swcli/swcli.py
+++ swcli/swcli.py
@@ -11,12 +11,11 @@ def films(id='', title=''):
     """
     Return a one or many movies on Star Wars trilogies by Title.
     """
-    if id != '' and title == '':
-        response = GetFilm.get_film_by_id(id)
-    elif title != '' and id == '':
-        response = GetFilm.get_film_by_title(title)
-
     try:
+        if id != '' and title == '':
+            response = GetFilm.get_film_by_id(id)
+        elif title != '' and id == '':
+            response = GetFilm.get_film_by_title(title)
         return response
     except BaseException:
         raise SystemExit('Invalid search!')
@@ -27,12 +26,11 @@ def people(id='', name=''):
     Returns a character on the Star Wars movies.
     Like: Luke, Leia, Anakin, etc.
     """
-    if id != '' and name == '':
-        response = GetPerson.get_person_by_id(id)
-    elif name != '' and id == '':
-        response = GetPerson.get_person_by_name(name)
-
     try:
+        if id != '' and name == '':
+            response = GetPerson.get_person_by_id(id)
+        elif name != '' and id == '':
+            response = GetPerson.get_person_by_name(name)
         return response
     except BaseException:
         raise SystemExit('Invalid search!')
@@ -43,12 +41,11 @@ def planets(id='', name=''):
     Return a planet.
     Like: Hoth, Naboo, etc.
     """
-    if id != '' and name == '':
-        response = GetPlanet.get_planet_by_id(id)
-    elif name != '' and id == '':
-        response = GetPlanet.get_planets_by_name(name)
-
     try:
+        if id != '' and name == '':
+            response = GetPlanet.get_planet_by_id(id)
+        elif name != '' and id == '':
+            response = GetPlanet.get_planets_by_name(name)
         return response
     except BaseException:
         raise SystemExit('Invalid search!')
@@ -59,12 +56,11 @@ def species(id='', name=''):
     Return a species on the Star Wars universe.
     Like: Wookie, Human, etc.
     """
-    if id != '' and name == '':
-        response = GetSpecie.get_specie_by_id(id)
-    elif name != '' and id == '':
-        response = GetSpecie.get_specie_by_name(name)
-
     try:
+        if id != '' and name == '':
+            response = GetSpecie.get_specie_by_id(id)
+        elif name != '' and id == '':
+            response = GetSpecie.get_specie_by_name(name)
         return response
     except BaseException:
         raise SystemExit('Invalid search!')
@@ -75,12 +71,11 @@ def starships(id='', name=''):
     Return a Starships on Star Wars universe.
     Like: Death Star, Millenium Falcon, etc.
     """
-    if id != '' and name == '':
-        response = GetStarship.get_starship_by_id(id)
-    elif name != '' and id == '':
-        response = GetStarship.get_starship_by_name(name)
-
     try:
+        if id != '' and name == '':
+            response = GetStarship.get_starship_by_id(id)
+        elif name != '' and id == '':
+            response = GetStarship.get_starship_by_name(name)
         return response
     except BaseException:
         raise SystemExit('Invalid search!')
@@ -91,18 +86,17 @@ def vehicles(id='', name=''):
     Return a vehicle used on Star Wars universe.
     Like: TIE Fighter, Geonosian starfighter, etc.
     """
-    if id != '' and name == '':
-        response = GetVehicle.get_vehicle_by_id(id)
-    elif name != '' and id == '':
-        response = GetVehicle.get_vehicle_by_name(name)
-
     try:
+        if id != '' and name == '':
+            response = GetVehicle.get_vehicle_by_id(id)
+        elif name != '' and id == '':
+            response = GetVehicle.get_vehicle_by_name(name)
         return response
     except BaseException:
         raise SystemExit('Invalid search!')
 
 
-if __name__ == '__main__':
+def main():
     fire.Fire({
         'films': films,
         'people': people,
@@ -111,3 +105,7 @@ if __name__ == '__main__':
         'starships': starships,
         'vehicles': vehicles,
     })
+
+
+if __name__ == '__main__':
+    main()
